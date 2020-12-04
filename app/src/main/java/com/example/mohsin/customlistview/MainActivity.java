f5427d412eed744b88153c816246edfbbe55e53d
Dhruv Rawat <dhruv@pankhuri.co>
2020-12-04 14:59:45 +0530

    android midterm

--- app/src/main/java/com/example/mohsin/customlistview/MainActivity.java
+++ app/src/main/java/com/example/mohsin/customlistview/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mohsin.customlistview;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -19,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     ListView listView;
     String mTitle[] = {"Jagdish", "Tushar", "Himanshu", "Jayant", "Jay"};
-    String mDescription[] = {"Hi friends", "Hello bro", "I am the best", "Love my life", "i rock!"};
-    int images[] = {R.drawable.jagdish, R.drawable.tushar, R.drawable.himanshu, R.drawable.jayant, R.drawable.jay};
+    String mDescription[] = {"21", "21", "21", "21", "22"};
+    String gender[] = {"male","male","male","male","male" };
 
 
     @Override
@@ -29,26 +31,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.listView);
+        Button button = findViewById(R.id.add);
         // now create an adapter class
-
-        MyAdapter adapter = new MyAdapter(this, mTitle, mDescription, images);
+        MyAdapter adapter = new MyAdapter(this, mTitle, mDescription, gender);
         listView.setAdapter(adapter);
 
     }
 
+    public void addFriends(){
+        Intent i = new Intent(MainActivity.this, Form.class );
+        startActivity(i);
+    }
     class MyAdapter extends ArrayAdapter<String> {
 
         Context context;
         String rTitle[];
         String rDescription[];
-        int rImgs[];
+        String rGender[];
 
-        MyAdapter (Context c, String title[], String description[], int imgs[]) {
+        MyAdapter (Context c, String title[], String description[], String gender[]) {
             super(c, R.layout.row, R.id.textView1, title);
             this.context = c;
             this.rTitle = title;
             this.rDescription = description;
-            this.rImgs = imgs;
+            this.rGender = gender;
 
         }
 
@@ -57,14 +63,15 @@ public class MainActivity extends AppCompatActivity {
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
             LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View row = layoutInflater.inflate(R.layout.row, parent, false);
-            ImageView images = row.findViewById(R.id.image);
             TextView myTitle = row.findViewById(R.id.textView1);
             TextView myDescription = row.findViewById(R.id.textView2);
+            TextView gender = row.findViewById(R.id.gender);
 
             // now set our resources on views
-            images.setImageResource(rImgs[position]);
             myTitle.setText(rTitle[position]);
             myDescription.setText(rDescription[position]);
+            gender.setText(rGender[position]);
+
 
 
 
